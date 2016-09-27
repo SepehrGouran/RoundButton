4460ae91fb1a992a25f30bae91a8ffdb2b0a008d
Sepehr Gouran <SepehrGouran@users.noreply.github.com>
2016-09-27 16:43:59 +0330

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+      Icon icon = new ImageIcon("10.png");
+      JButton roundedButton = new RoundedButton("Windows 10", icon, "10.png");
+	}
+}
